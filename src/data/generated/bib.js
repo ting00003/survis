9a9ae8298e4af3e024f4d41cd5f32c37c13ecf6c
ting00003 <tingfangxa@gmail.com>
2025-05-13 18:12:22 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,42 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+    "noauthor_comparative_nodate": {
+        "title": "A comparative study of algorithmic\u2013user classification practices in online dating: a human\u2013machine learning process",
+        "type": "article"
+    },
+    "noauthor_construction_nodate": {
+        "title": "The construction of matches in dating platforms.",
+        "type": "article"
+    },
+    "noauthor_enhancing_nodate": {
+        "title": "Enhancing a user matchmaking algorithm using {Personalized} {PageRank}",
+        "type": "article"
+    },
+    "noauthor_finding_nodate": {
+        "title": "Finding love on a first data: {Matching} algorithms in online dating.",
+        "type": "article"
+    },
+    "noauthor_folk_nodate": {
+        "title": "Folk {Theories} and {User} {Strategies} on {Dating} {Apps}: {How} {Users} {Understand} and {Manage} {Their} {Experience} with {Algorithmic} {Matchmaking}.",
+        "type": "article"
+    },
+    "noauthor_matching_nodate": {
+        "title": "Matching theory-based recommender systems in online dating",
+        "type": "article"
+    },
+    "noauthor_social_nodate": {
+        "title": "A social matching system for an online dating network: {A} preliminary study",
+        "type": "article"
+    },
+    "noauthor_survey_nodate": {
+        "title": "A survey on conversational recommender systems.",
+        "type": "article"
+    },
+    "noauthor_testing_nodate": {
+        "title": "Testing how dating apps recommend a potential matches for user.",
+        "type": "article"
+    },
+    "noauthor_what_nodate": {
+        "title": "What {You} {Like}, {What} {I} {Am}: {Online} {Dating} {Recommendation} via {Matching} {Individual} {Preferences} {With} {Features}.",
+        "type": "article"
     }
 };
